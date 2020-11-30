e0845fe8e1d76a0d3298fb39b97b5d961375ba00
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2020-11-30 18:41:32 -0300

    Update
    interatividade

--- First_Flutter_App/main.dart
+++ First_Flutter_App/main.dart
@@ -84,10 +84,7 @@ Widget titleSection = Container(
             ],
           ),
       ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
+      FavoriteWidget(),
     ],
   ),
 );
@@ -112,4 +109,53 @@ Column _buildButtomColumn(Color color, IconData icon, String label){
     ],
   );
 }
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = false;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.amber,
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite(){
+    setState(() {
+      if(_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
+
 
